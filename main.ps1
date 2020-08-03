3304f2ae8ade188b06dd70fe039dc5518c90d3f6
Malcolm Anderson <Malcolm.Anderson@kp.org>
2020-08-03 11:51:26 -0700

    test file for connection

--- main.ps1
+++ main.ps1
@@ -7,18 +7,81 @@ if($args){
 # duplicated logic with fileName 
 $sourceFileName = "cfAppsData_" + $fileIdentifier + ".txt"
 $targetFileName = "App_Results_" + $fileIdentifier + ".txt"
-$serviceSourceFile = "_ServiceNames_2.txt"
+$serviceSourceFile = "_ServiceNames.txt"
 
 Write-Host "Writing start applications to data file: " $sourceFileName
 $cmd = "cf apps | Select-String -Pattern started > $sourceFileName"
 Invoke-Expression $cmd
 
 
+# ForEach ($serviceName in (Get-Content -Path $serviceSourceFile)){
+#     $cmd = "./PrintServiceStatus " `
+#             + " -sourceFileName " + $sourceFileName `
+#             + " -targetFileName " + $targetFileName `
+#             + " -serviceName " + $serviceName
+#     write-host $cmd
+#     Invoke-Expression $cmd
+#  }
+
+
+
+# if target file exists, delete it.
+if(Test-Path $targetFileName -PathType Leaf){
+    Remove-Item -Path $targetFileName
+}
+
+
 ForEach ($serviceName in (Get-Content -Path $serviceSourceFile)){
-    $cmd = "./PrintServiceStatus " `
-            + " -sourceFileName " + $sourceFileName `
-            + " -targetFileName " + $targetFileName `
-            + " -serviceName " + $serviceName
+     $cmd = "./PrintServiceStatus " `
+             + " -sourceFileName " + $sourceFileName `
+             + " -targetFileName " + $targetFileName `
+             + " -serviceName " + $serviceName
     write-host $cmd
-    Invoke-Expression $cmd
- }
+    $safeToContinue = Invoke-Expression $cmd
+    Write-Host $safeToContinue
+
+    if($safeToContinue){
+        # "data_" + yyyyMMdd_HHmm + "_ServiceName_Configuration.txt"
+        $serviceConfigurationData_FileName = "data_" + $fileIdentifier + "_" + $ServiceName + "_Configuration.txt"
+        Write-Host "Writing service configuration data file: " $serviceConfigurationData_FileName
+        $cmd = "cf env $serviceName > $serviceConfigurationData_FileName"
+        Invoke-Expression $cmd
+
+        $cmdStem = "./PrintConfigurationStatus " `
+             + " -sourceFileName " + $serviceConfigurationData_FileName `
+             + " -targetFileName " + $targetFileName `
+             + " -configurationName " 
+        $cmd = $cmdStem + "level"
+        write-host $cmd
+        Invoke-Expression $cmd
+
+        $cmd = $cmdStem + "Auto-Scaling"
+        write-host $cmd
+        Invoke-Expression $cmd
+        
+        $cmd = $cmdStem + "ENABLE_OAUTH:"
+        write-host $cmd
+        Invoke-Expression $cmd
+
+        $cmd = $cmdStem + "oauthServer.basicAuth.username"
+        write-host $cmd
+        Invoke-Expression $cmd
+
+        $cmd = $cmdStem + "GIT_BRANCH:"
+        write-host $cmd
+        Invoke-Expression $cmd
+        
+        $cmd = $cmdStem + "CONFIG_PROFILE:"
+        write-host $cmd
+        Invoke-Expression $cmd
+        
+        $cmd = $cmdStem + "CONFIG_LABEL:"
+        write-host $cmd
+        Invoke-Expression $cmd
+
+        # Add separater for next group
+        Add-Content -Path $targetFileName ""
+     } else { 
+        Add-Content -Path $targetFileName ""
+     }
+}
